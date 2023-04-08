9ef1effb4584a43169b45f343c3ba36222b34a43
Santosh Kumar Patro <santoshkpatro11@gmail.com>
2023-04-08 21:05:38 +0530

    added product model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    is_available = models.BooleanField(default=True)
+    stock = models.IntegerField(default=0)
+
+    # Timestamp
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
